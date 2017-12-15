99b78c2008529e42650cea8d946f6564cb6fd390
liutkevych <oleksandr.liutkevych@seniordevonly.com>
2017-12-15 12:08:55 +0200

    Add POST /todos and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    trim: true
+  }
+});
+
+module.exports = {User};
