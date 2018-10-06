abad8ea2b85dfa929d02b4732904420567f0cbdd
liutkevych <oli@keepit.com>
2018-10-06 22:32:27 +0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -133,4 +133,12 @@ app.listen(port, () => {
   console.log(`Started listen on port ${port}`);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 module.exports = {app};
